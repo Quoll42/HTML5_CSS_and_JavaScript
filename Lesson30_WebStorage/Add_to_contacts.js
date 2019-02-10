d01b4f432bed1f21193c34537b018a743199fd34
Quoll42 <Quoll42@users.noreply.github.com>
2019-02-10 22:59:36 +0800

    Lesson30: WebStorage, got contacts  stored+loaded  in Web Storage API

--- Lesson30_WebStorage/Add_to_contacts.js
+++ Lesson30_WebStorage/Add_to_contacts.js
@@ -1,4 +1,17 @@
+
+/* The 1st version of the function below would work to store 1 contact */ 
 function store(contact) {
     var c = JSON.stringify(contact);
-    localStorage.setItem('contacts',c);
+    localStorage.setItem('contact',c);
+}
+
+/* This 2nd version of the function works for storing multiple contacts */ 
+function store(contact) {
+    var contactsStored = localStorage.getItem('contacts');
+    var contacts = [];
+    if (contactsStored) {
+        contacts = JSON.parse(contactsStored);
+    }
+    contacts.push(contact);
+    localStorage.setItem('contacts',JSON.stringify(contacts));
 }
