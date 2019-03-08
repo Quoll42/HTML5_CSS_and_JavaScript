9109dbb772ebe270eb261515528410c29e720171
Quoll42 <Quoll42@users.noreply.github.com>
2019-03-08 21:59:58 +0800

    Lesson38: LocationAPI, finished 'locate.html'

--- Lesson38_LocationAPI/simpleShowLocationFunction.js
+++ Lesson38_LocationAPI/simpleShowLocationFunction.js
@@ -1,6 +1,11 @@
-navigator.geolocation.getCurrentPosition( function(location) {
-    console.log('Latitude: ' + location.coords.latitude);
-    console.log('Longitude: ' + location.coords.longitude);
-    console.log('Altitude: ' + location.coords.altitude);
-    console.log('Accuracy: ' + location.coords.accuracy + ' <= important');
-});
+navigator.geolocation.getCurrentPosition(
+    function(location) { //1st parameter: success callback
+        console.log('Latitude: '  + location.coords.latitude);
+        console.log('Longitude: ' + location.coords.longitude);
+        console.log('Altitude: ' + location.coords.altitude);
+        console.log('Accuracy: ' + location.coords.accuracy + ' <= important');
+    }
+,
+    function(error)     //2nd parameter: optional error callback
+        {console.log(error)}
+);
